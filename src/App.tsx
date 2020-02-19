f6f45fe05bae860e7048dcde5a1801cf5ce703f0
Lancy Goyal <i.lancygoyal@gmail.com>
2020-02-19 10:52:30 +0530

    Electron and Material UI Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
       </header>
     </div>
   );
