e5a796d9a2c7dda3d850ede91cace5176e33b511
Lancy Goyal <i.lancygoyal@gmail.com>
2020-03-05 07:29:24 +0530

    Add Bill

--- src/components/daybook/addDialog.tsx
+++ src/components/daybook/addDialog.tsx
@@ -9,11 +9,28 @@ import { Formik } from "formik";
 import * as Yup from "yup";
 import { Grid, Theme, makeStyles, createStyles } from "@material-ui/core";
 import { ALPHA_SPACE_DOT } from "../../constants/regex";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import BillTable from "./billTable";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     form: {
       width: "100%"
+    },
+    tabBtn: {
+      width: "50%",
+      borderRadius: 0,
+      borderBottomStyle: "solid",
+      borderBottomWidth: 5,
+      borderBottomColor: theme.palette.primary.main
+    },
+    tabBtnActive: {
+      width: "50%",
+      borderRadius: 0,
+      borderBottomStyle: "solid",
+      borderBottomWidth: 5,
+      borderBottomColor: theme.palette.background.default,
+      color: theme.palette.background.default
     },
     btn: {
       margin: 5,
@@ -23,8 +40,116 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+const top100Films = [
+  { title: "The Shawshank Redemption", year: 1994 },
+  { title: "The Godfather", year: 1972 },
+  { title: "The Godfather: Part II", year: 1974 },
+  { title: "The Dark Knight", year: 2008 },
+  { title: "12 Angry Men", year: 1957 },
+  { title: "Schindler's List", year: 1993 },
+  { title: "Pulp Fiction", year: 1994 },
+  { title: "The Lord of the Rings: The Return of the King", year: 2003 },
+  { title: "The Good, the Bad and the Ugly", year: 1966 },
+  { title: "Fight Club", year: 1999 },
+  { title: "The Lord of the Rings: The Fellowship of the Ring", year: 2001 },
+  { title: "Star Wars: Episode V - The Empire Strikes Back", year: 1980 },
+  { title: "Forrest Gump", year: 1994 },
+  { title: "Inception", year: 2010 },
+  { title: "The Lord of the Rings: The Two Towers", year: 2002 },
+  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
+  { title: "Goodfellas", year: 1990 },
+  { title: "The Matrix", year: 1999 },
+  { title: "Seven Samurai", year: 1954 },
+  { title: "Star Wars: Episode IV - A New Hope", year: 1977 },
+  { title: "City of God", year: 2002 },
+  { title: "Se7en", year: 1995 },
+  { title: "The Silence of the Lambs", year: 1991 },
+  { title: "It's a Wonderful Life", year: 1946 },
+  { title: "Life Is Beautiful", year: 1997 },
+  { title: "The Usual Suspects", year: 1995 },
+  { title: "Léon: The Professional", year: 1994 },
+  { title: "Spirited Away", year: 2001 },
+  { title: "Saving Private Ryan", year: 1998 },
+  { title: "Once Upon a Time in the West", year: 1968 },
+  { title: "American History X", year: 1998 },
+  { title: "Interstellar", year: 2014 },
+  { title: "Casablanca", year: 1942 },
+  { title: "City Lights", year: 1931 },
+  { title: "Psycho", year: 1960 },
+  { title: "The Green Mile", year: 1999 },
+  { title: "The Intouchables", year: 2011 },
+  { title: "Modern Times", year: 1936 },
+  { title: "Raiders of the Lost Ark", year: 1981 },
+  { title: "Rear Window", year: 1954 },
+  { title: "The Pianist", year: 2002 },
+  { title: "The Departed", year: 2006 },
+  { title: "Terminator 2: Judgment Day", year: 1991 },
+  { title: "Back to the Future", year: 1985 },
+  { title: "Whiplash", year: 2014 },
+  { title: "Gladiator", year: 2000 },
+  { title: "Memento", year: 2000 },
+  { title: "The Prestige", year: 2006 },
+  { title: "The Lion King", year: 1994 },
+  { title: "Apocalypse Now", year: 1979 },
+  { title: "Alien", year: 1979 },
+  { title: "Sunset Boulevard", year: 1950 },
+  {
+    title:
+      "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
+    year: 1964
+  },
+  { title: "The Great Dictator", year: 1940 },
+  { title: "Cinema Paradiso", year: 1988 },
+  { title: "The Lives of Others", year: 2006 },
+  { title: "Grave of the Fireflies", year: 1988 },
+  { title: "Paths of Glory", year: 1957 },
+  { title: "Django Unchained", year: 2012 },
+  { title: "The Shining", year: 1980 },
+  { title: "WALL·E", year: 2008 },
+  { title: "American Beauty", year: 1999 },
+  { title: "The Dark Knight Rises", year: 2012 },
+  { title: "Princess Mononoke", year: 1997 },
+  { title: "Aliens", year: 1986 },
+  { title: "Oldboy", year: 2003 },
+  { title: "Once Upon a Time in America", year: 1984 },
+  { title: "Witness for the Prosecution", year: 1957 },
+  { title: "Das Boot", year: 1981 },
+  { title: "Citizen Kane", year: 1941 },
+  { title: "North by Northwest", year: 1959 },
+  { title: "Vertigo", year: 1958 },
+  { title: "Star Wars: Episode VI - Return of the Jedi", year: 1983 },
+  { title: "Reservoir Dogs", year: 1992 },
+  { title: "Braveheart", year: 1995 },
+  { title: "M", year: 1931 },
+  { title: "Requiem for a Dream", year: 2000 },
+  { title: "Amélie", year: 2001 },
+  { title: "A Clockwork Orange", year: 1971 },
+  { title: "Like Stars on Earth", year: 2007 },
+  { title: "Taxi Driver", year: 1976 },
+  { title: "Lawrence of Arabia", year: 1962 },
+  { title: "Double Indemnity", year: 1944 },
+  { title: "Eternal Sunshine of the Spotless Mind", year: 2004 },
+  { title: "Amadeus", year: 1984 },
+  { title: "To Kill a Mockingbird", year: 1962 },
+  { title: "Toy Story 3", year: 2010 },
+  { title: "Logan", year: 2017 },
+  { title: "Full Metal Jacket", year: 1987 },
+  { title: "Dangal", year: 2016 },
+  { title: "The Sting", year: 1973 },
+  { title: "2001: A Space Odyssey", year: 1968 },
+  { title: "Singin' in the Rain", year: 1952 },
+  { title: "Toy Story", year: 1995 },
+  { title: "Bicycle Thieves", year: 1948 },
+  { title: "The Kid", year: 1921 },
+  { title: "Inglourious Basterds", year: 2009 },
+  { title: "Snatch", year: 2000 },
+  { title: "3 Idiots", year: 2009 },
+  { title: "Monty Python and the Holy Grail", year: 1975 }
+];
+
 export default ({ open, handleClose }) => {
   const classes = useStyles();
+  const [type, setType] = React.useState("in");
   const { t } = useTranslation();
 
   return (
@@ -33,12 +158,10 @@ export default ({ open, handleClose }) => {
       onClose={handleClose}
       aria-labelledby="enter-password-title"
       disableBackdropClick={true}
-      fullScreen={false}
       maxWidth="md"
-      style={{ maxHeight: "700px" }}
     >
       <DialogTitle id="enter-password-title">{t("app:addRecord")}</DialogTitle>
-      <DialogContent style={{ width: "500px" }}>
+      <DialogContent style={{ width: "700px", maxHeight: "500px" }}>
         <Formik
           initialValues={{
             firmName: "",
@@ -122,6 +245,60 @@ export default ({ open, handleClose }) => {
             return (
               <form className={classes.form} onSubmit={handleSubmit} noValidate>
                 <Grid container spacing={2}>
+                  <Grid
+                    container
+                    item
+                    xs={12}
+                    sm={12}
+                    justify="center"
+                    alignItems="center"
+                  >
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      className={
+                        type === "in" ? classes.tabBtnActive : classes.tabBtn
+                      }
+                      onClick={() => setType("in")}
+                    >
+                      {t("app:in")}
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      className={
+                        type === "out" ? classes.tabBtnActive : classes.tabBtn
+                      }
+                      onClick={() => setType("out")}
+                    >
+                      {t("app:out")}
+                    </Button>
+                  </Grid>
+                  <Grid item xs={8} sm={8}>
+                    <Autocomplete
+                      id="accountName"
+                      options={top100Films}
+                      getOptionLabel={option => option.title}
+                      clearOnEscape
+                      renderInput={params => (
+                        <TextField
+                          {...params}
+                          label="Account Name"
+                          variant="outlined"
+                          fullWidth
+                        />
+                      )}
+                    />
+                  </Grid>
+                  <Grid item xs={4} sm={4}>
+                    <TextField
+                      name="recordNo"
+                      variant="standard"
+                      fullWidth
+                      id="recordNo"
+                      value="10001"
+                    />
+                  </Grid>
                   <Grid item xs={12} sm={12}>
                     <TextField
                       name="firmName"
@@ -138,7 +315,6 @@ export default ({ open, handleClose }) => {
                       }
                       error={errors.firmName && touched.firmName}
                       inputProps={{ pattern: ALPHA_SPACE_DOT }}
-                      autoFocus
                     />
                   </Grid>
                   <Grid item xs={12} sm={12}>
@@ -191,6 +367,9 @@ export default ({ open, handleClose }) => {
                     />
                   </Grid>
                 </Grid>
+                <div style={{ maxWidth: "100%" }}>
+                  <BillTable />
+                </div>
                 <Grid
                   container
                   item
