3566a3065e47fa997c2b242f74cf5c6df6c95c37
Lancy Goyal <i.lancygoyal@gmail.com>
2020-03-13 15:01:54 +0530

    Prod Issue

--- public/electron.js
+++ public/electron.js
@@ -1,13 +1,6 @@
-const electron = require("electron");
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const { app, BrowserWindow, ipcMain, Menu } = require("electron");
 const path = require("path");
 const isDev = require("electron-is-dev");
-const {
-  default: installExtension,
-  REACT_DEVELOPER_TOOLS,
-  REDUX_DEVTOOLS
-} = require("electron-devtools-installer");
 
 let mainWindow;
 
@@ -20,14 +13,21 @@ function createWindow() {
       nodeIntegrationInWorker: true // <---  for web workers
     }
   });
+
   mainWindow.loadURL(
     isDev
       ? "http://localhost:3000"
       : `file://${path.join(__dirname, "../build/index.html")}`
   );
+
   if (isDev) {
+    const {
+      default: installExtension,
+      REACT_DEVELOPER_TOOLS,
+      REDUX_DEVTOOLS
+    } = require("electron-devtools-installer");
     // Open the DevTools.
-    //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
+    // BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
 
     installExtension(REACT_DEVELOPER_TOOLS)
       .then(name => console.log(`Added Extension:  ${name}`))
@@ -36,12 +36,43 @@ function createWindow() {
     installExtension(REDUX_DEVTOOLS)
       .then(name => console.log(`Added Extension:  ${name}`))
       .catch(err => console.log("An error occurred: ", err));
+
     mainWindow.webContents.openDevTools();
   }
+
   mainWindow.on("closed", () => (mainWindow = null));
 }
 
-app.on("ready", createWindow);
+function generateMenu() {
+  const template = [
+    {
+      label: "App",
+      submenu: [
+        { role: "about", label: "Info" },
+        { role: "quit", label: "Exit" }
+      ]
+    },
+    {
+      label: "Dev",
+      submenu: [
+        { role: "reload" },
+        { role: "forcereload" },
+        { role: "toggledevtools" }
+      ]
+    },
+    {
+      role: "window",
+      submenu: [{ role: "minimize" }, { role: "close" }]
+    }
+  ];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+}
+
+app.on("ready", () => {
+  createWindow();
+  generateMenu();
+});
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import EnRoute from "../components/enRoute";
 import SignIn from "../pages/signin";
 import SignUp from "../pages/signup";
